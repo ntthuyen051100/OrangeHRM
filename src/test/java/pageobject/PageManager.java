a18bb2e6588cb33c1304ead1cf8534f97c5c46c9
Thu Huyen <ntthuyen051100@gmail.com>
2026-08-12 01:17:04 +0700

    Add memo in PageManager.java, SideMenuTest.java

--- src/test/java/pageobject/PageManager.java
+++ src/test/java/pageobject/PageManager.java
@@ -9,6 +9,21 @@ nhiều browser DriverFactory*/
 public class PageManager {
 /*Sử dụng kỹ thuật Lazy Initialization qua hàm Getter.
 Mỗi khi gọi page.loginPage(), một instance mới đi kèm driver chuẩn của luồng sẽ được sinh ra*/
+/*Tại đây sẽ KHÔNG KHAI BÁO Driver gì cả, không giống như các class Page khác sẽ là
+    Webdriver driver;
+    ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+Cũng không extends BasePage để dùng constructor khởi tạo cứng driver này => chắc chắn sẽ dính lỗi NullPointerException ngay lập tức.
+    public AdminPage(WebDriver driver) {super(driver);}
+
+=> Dù có khai báo biến ngoài @Test (đồng nghĩa với sẽ tạo biến trước cả @BeforeMethod của BaseTest (chứa việc khởi tạo
+driver) thì
+    - Khi TestNG load class AdminTest_V5_...: Nó thực thi lệnh new PageManager(). Lúc này Java chỉ cấp phát một vùng nhớ
+cho đối tượng PageManager. Nó KHÔNG hề chạy các hàm bên trong như loginPage() hay commonPage().
+     - Khi vào đến @BeforeMethod: Hệ thống chạy DriverFactory.setDriver(browser) → Lúc này Driver mới chính thức được tạo
+và đưa vào ThreadLocal.
+     - Khi vào đến @Test: Bạn gọi page.loginPage(). Lúc này dòng lệnh (1) mới chính thức kích hoạt. Nó đi tìm
+DriverFactory.getDriver(). Vì @BeforeMethod đã chạy trước đó rồi, nên lúc này Driver đã tồn tại → Kết quả là KHÔNG BỊ NULL.
+*/
 
 /*    Tùy vào mình muốn chạy parallel trên driver tiêu chuẩn là chrome thì dùng DriverManager,
 nếu muốn dùng browser khác thì dùng DriverFactory*/
